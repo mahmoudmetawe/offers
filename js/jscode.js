7877ad3f5025a191a37445a5c80d515bbc0d1048
mahmoudmetawe <mmetawe12@yahoo.com>
2024-03-09 06:27:59 -0800

    swal

--- js/jscode.js
+++ js/jscode.js
@@ -1,8 +1,26 @@
-let done = document.querySelector('#done');
-
-done.onclick = function()
-{
-    done.classList.remove("cilck")
+function sweet(){
+  Swal.fire({
+    html: `
+    <img src="pic/sweet alert/Group 1000005398.png" alt="">,
+    <br>
+    <br>
+    <h4>تم تسجيل دخولك بنجاح</h4>
+    `,
+    showConfirmButton: false,
+    timer : 3500,
+    width : 250,
+  });
 }
 
-console.log(done.classList);
+function sweet2(){
+  swal.fire(
+    {
+      icon:"success",
+      title : "تم اضافة العرض بنجاح",
+      text : "لقد تم اضافة العرض الخاص بك بنجاح .. يمكنك بكل سهولة معرفة تفاصيل العرض و تعديل بياناته بكل سهولة",
+      timer : 3500 ,
+      width : 350,
+      showConfirmButton: false,
+    }
+  );
+}
